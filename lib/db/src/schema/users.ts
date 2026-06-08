061c465b7c3946f0b1113c74a61948448ff03c36
Durga Prasad Cheppala <24211a05a0@bvrit.ac.in>
2026-06-08 23:03:54 +0530

    Update users.ts

--- lib/db/src/schema/users.ts
+++ lib/db/src/schema/users.ts
@@ -11,5 +11,5 @@ export const usersTable = pgTable("users", {
 });
 
 export const insertUserSchema = createInsertSchema(usersTable).omit({ id: true, createdAt: true });
-export type InsertUser = z.infer<typeof insertUserSchema>;
+export type InsertUser = any;
 export type User = typeof usersTable.$inferSelect;
